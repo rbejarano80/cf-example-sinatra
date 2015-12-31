d6e0d3334bc0e3a31e8b59087c6884b070cae11c
Juan Pablo Genovese <juanpgenovese@gmail.com>
2015-12-31 11:16:08 -0300

    Add user provided service

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require "sinatra/reloader"
+require "json"
 
 configure do
   set :views,         File.dirname(__FILE__) + '/views'
@@ -11,6 +12,9 @@ get '/' do
   @host = ENV['CF_INSTANCE_IP'] || "192.168.0.0"
   @port = ENV['CF_INSTANCE_PORT'] || "9292"
   @index = ENV['CF_INSTANCE_INDEX'] || "0"
+
+  vcap_services = JSON.parse(ENV['VCAP_SERVICES'])
+  @my_fictional_service = vcap_services['user-provided'].first
 
   erb :index
 end
