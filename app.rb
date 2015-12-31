112a9d2e30f38d4d56f2d1b93671907b0d022b72
Juan Pablo Genovese <juanpgenovese@gmail.com>
2015-12-31 09:33:50 -0300

    Add service for demonstration

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 require "sinatra/reloader"
+require "json"
+require "mysql2"
+
 
 configure do
   set :views,         File.dirname(__FILE__) + '/views'
@@ -11,6 +14,15 @@ get '/' do
   @host = ENV['CF_INSTANCE_IP'] || "192.168.0.0"
   @port = ENV['CF_INSTANCE_PORT'] || "9292"
   @index = ENV['CF_INSTANCE_INDEX'] || "0"
+
+  vcap_services = JSON.parse(ENV['VCAP_SERVICES'])
+  db_user = vcap_services['p-mysql'].first['credentials']['username']
+  db_password = vcap_services['p-mysql'].first['credentials']['password']
+  db_host = vcap_services['p-mysql'].first['credentials']['hostname']
+  @db_name = vcap_services['p-mysql'].first['credentials']['name']
+
+  mysql_client = Mysql2::Client.new(host: db_host, username: db_user, password: db_password)
+  @query_result = mysql_client.query("SELECT table_name FROM INFORMATION_SCHEMA.TABLES")
 
   erb :index
 end
